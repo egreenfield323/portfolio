a30a74a05f5a91cd36aebd851ffbc7da7930dfa8
Evan Greenfield <evangreenfield@live.com>
2023-12-08 21:02:38 -0500

    Add programming section

--- js/page_logic.js
+++ js/page_logic.js
@@ -32,24 +32,29 @@ function shiftToGalleryNavBar() {
     experience.text("About");
 
     var education = $("a[href='#education']");
-    education.attr("href", "#video");
-    education.text("Video");
+    education.attr("href", "#programming");
+    education.text("Programming");
 
     var interests = $("a[href='#interests']");
-    interests.attr("href", "#music");
-    interests.text("Music");
-
+    interests.attr("href", "#video");
+    interests.text("Video");
+    
     var skills = $("a[href='#skills']");
-    skills.attr("href", "#animation");
-    skills.text("Animation");
-
+    skills.attr("href", "#music");
+    skills.text("Music");
+    
     var extra1 = $("a[href='#extra1']");
-    extra1.attr("href", "#service");
-    extra1.text("Service");
+    extra1.attr("href", "#animation");
+    extra1.text("Animation");
+    
+    var extra2 = $("a[href='#extra2']");
+    extra2.attr("href", "#service");
+    extra2.text("Service");
 
-    var extra2 = $("a[href='#skills']");
-    extra2.attr("href", "#school");
-    extra2.text("School Activities");
+    var extra3 = $("a[href='#extra3']");
+    extra3.attr("href", "#school");
+    extra3.text("School Activities");
+
 }
 
 function shiftToResumeNavBar() {
@@ -57,24 +62,28 @@ function shiftToResumeNavBar() {
     about.attr("href", "#experience");
     about.text("Experience");
 
+    var programming = $("a[href='#programming']");
+    programming.attr("href", "#education");
+    programming.text("Education");
+
     var video = $("a[href='#video']");
-    video.attr("href", "#education");
-    video.text("Education");
+    video.attr("href", "#interests");
+    video.text("Interests");
 
     var music = $("a[href='#music']");
-    music.attr("href", "#interests");
-    music.text("Interests");
+    music.attr("href", "#skills");
+    music.text("Skills");
 
     var animation = $("a[href='#animation']");
-    animation.attr("href", "#skills");
-    animation.text("Skills");
+    animation.attr("href", "#extra1");
+    animation.text("");
 
     var service = $("a[href='#service']");
-    service.attr("href", "#extra1");
+    service.attr("href", "#extra2");
     service.text("");
 
     var school = $("a[href='#school']");
-    school.attr("href", "#extra2");
+    school.attr("href", "#extra3");
     school.text("");
 
 }
